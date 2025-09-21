8d42dd48f0bc0c5e0584b76feb580e0d09dfa9a4
Willian Brendo <willianbrendo.dev@gmail.com>
2025-09-21 12:31:17 -0300

    Seller entity class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,25 @@
 package app;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Department obj = new Department(1, "Books");
+		
+		
+		
+		Seller seller = new Seller(1, "Willian Brendo", "willianbrendo.dev@gmail.com", new Date(), 2800.00, obj);
+		
+		
+		System.out.println(seller);
 
+				
+				
 	}
 
 }
