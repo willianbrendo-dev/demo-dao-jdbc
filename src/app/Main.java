8e52fafc551d1c91fd9b46db1a2ff5803f8fb18c
Willian Brendo <willianbrendo.dev@gmail.com>
2025-09-21 13:10:36 -0300

    SellerDaoJDBC and DaoFactory

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,8 @@ package app;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,8 @@ public class Main {
 		
 		Seller seller = new Seller(1, "Willian Brendo", "willianbrendo.dev@gmail.com", new Date(), 2800.00, obj);
 		
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
